b13b05ec0d15749433189bcd21b5c057da117497
maziyargh83 <mazyar.gh83@gmail.com>
2023-01-30 13:33:39 +0330

    update font

--- app/components/Logs/Logs.tsx
+++ app/components/Logs/Logs.tsx
@@ -7,11 +7,11 @@ interface LogsType {
 }
 export const Logs = ({ icon: Icon, title }: LogsType) => {
   return (
-    <div className="inline-flex items-center  rtl:ml-5 bg-white rounded-full p-2 ">
-      <div className="bg-purple-P5 rounded-[100px] mr-2">
+    <div className="inline-flex items-center flex-col md:flex-row md:min-h-[auto] min-h-[110px] rtl:ml-5 bg-white rounded-full p-2 ">
+      <div className="bg-purple-P5 rounded-[100px] rtl:md:ml-2 ltr:md:mr-2">
         <Icon />
       </div>
-      <span className="text-gray-G5 text-lg font-medium whitespace-nowrap">
+      <span className="text-gray-G5 text-[9px] md:text-lg font-medium text-center md:mt-0 mt-1 md:whitespace-nowrap">
         {t(title)}
       </span>
     </div>
